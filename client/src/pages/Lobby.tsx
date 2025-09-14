3b721a39c46335fc25a3aff06c843d124c30d49b
almonzer-fadl <valmonzer@protn.me>
2025-09-14 05:53:14 +0300

    t

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState, createContext } from "react";
+import { useEffect, useState, createContext } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import Menu from "./lobbyPages/Menu";
 import Game from "./lobbyPages/Game";
 import Complete from "./lobbyPages/Complete";
 import type { RoomData } from "../types/types";
+import socket from "../services/Socket";
 
 export const RoomContext = createContext<any>({});
 
@@ -15,7 +16,18 @@ export default function Lobby() {
   const [gamePhase, setGamePhase] = useState("menu");
 
   useEffect(() => {
-    console.log("unstateful: " + data + "room data: " + roomData);
+    console.log("Initial room data:", data);
+    
+    // Listen for room updates (when players join/leave)
+    socket.on("room-updated", (updatedRoomData: RoomData) => {
+      console.log("Room updated:", updatedRoomData);
+      setRoomData(updatedRoomData);
+    });
+
+    // Cleanup listener on unmount
+    return () => {
+      socket.off("room-updated");
+    };
   }, []);
 
   const { roomId } = useParams();
@@ -33,7 +45,7 @@ export default function Lobby() {
   return (
     <RoomContext.Provider value={roomData}>
       {gamePhase === "menu" && <Menu next={startGame}></Menu>}
-      {gamePhase === "game" && <Game next={endGame} roomId={roomId}></Game>}
+      {gamePhase === "game" && <Game next={endGame} roomId={roomId || ""}></Game>}
       {gamePhase === "complete" && <Complete next={newGame}></Complete>}
     </RoomContext.Provider>
   );
