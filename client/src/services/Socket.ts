dc3d5ef3f2f1130c0aa516603b4ecaceb467afc4
Mohannad-Algaali <hannodi.di@gmail.com>
2025-09-15 12:55:00 +0800

    fix vercel

--- client/src/services/Socket.ts
+++ client/src/services/Socket.ts
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io(
-  import.meta.env.VITE_BACKEND_ROUTE //|| "http://localhost:3000"
-);
+const socket = io(import.meta.env.VITE_BACKEND_ROUTE, {
+  transports: ["polling"],
+});
 
 socket.on("connected", (text: string) => {
   console.log("connected to socket : ", text);
