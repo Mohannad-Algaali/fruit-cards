b285552515f28d29fb4c0cd8d128fe30229d9c18
Mohannad-Algaali <hannodi.di@gmail.com>
2025-09-15 11:47:10 +0800

    leaving room incomplete

--- client/src/services/Socket.ts
+++ client/src/services/Socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io(
+  import.meta.env.VITE_BACKEND_ROUTE //|| "http://localhost:3000"
+);
 
 socket.on("connected", (text: string) => {
   console.log("connected to socket : ", text);
