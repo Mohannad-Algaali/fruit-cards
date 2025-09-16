68ca2f678662394b00c776b3678c5849260a9b8b
Mohannad-Algaali <hannodi.di@gmail.com>
2025-09-16 10:30:51 +0800

    temp fix

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_ROUTE, "http://localhost:5173"],
+    origin: "*", //[process.env.FRONTEND_ROUTE, "http://localhost:5173"],
     methods: ["GET", "POST"],
   },
   transports: ["websocket", "polling"],
